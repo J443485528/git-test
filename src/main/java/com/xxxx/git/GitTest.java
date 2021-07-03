4675608bcb3718e12c31a89c0cab4acf5dc0b09d
jlb <443485528@qq.com>
2021-07-03 16:37:08 +0800

    Third Commit

--- src/main/java/com/xxxx/git/GitTest.java
+++ src/main/java/com/xxxx/git/GitTest.java
@@ -2,7 +2,8 @@ package com.xxxx.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello Git!");
-        System.out.println("Hello Git!");
+        System.out.println("Hello Git1!");
+        System.out.println("Hello Git2!");
+        System.out.println("Hello Git3!");
     }
 }
